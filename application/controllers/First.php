8d13b82aa86525600911bf214881dce011108fd4
jerrybeanman <jerry1bean@hotmail.com>
2016-02-02 17:18:24 -0800

    added sleep, fixed first link

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -32,7 +32,19 @@ class First extends Application {
 
 		$this->render();
 	}
+        function zzz()
+        {
+            $this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array();
+		$this->data['authors'] = $authors;
 
+		$record = $this->quotes->get(1);
+		$this->data = array_merge($this->data, $record);
+
+		$this->render();
+        }
 }
 
 /* End of file Welcome.php */
